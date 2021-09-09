d4bba46573b78ba6e6c063110c2c39e9075a6631
Tukayama <ya.tumanov2009@yandex.ru>
2021-09-09 20:27:28 +0300

    init commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 2_000_000_000;
+        int enrollment = 500_000_000;
+        int balance =score + enrollment;
+        System.out.println(balance);
+
+    }
+}
